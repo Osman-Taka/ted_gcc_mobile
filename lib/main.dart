17b042cd8bd038b6a532a90517a3e9c98d1afc75
Osman Taka <85412764+Osmanttne@users.noreply.github.com>
2022-03-12 15:18:14 +0300

    themes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
+        theme: ThemeData.dark(
           primarySwatch: Colors.deepPurple,
         ),
       home: ArticleList(Articles.FetchAll())
