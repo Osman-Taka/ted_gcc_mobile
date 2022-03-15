c2af1fb82b0ce5c3b79cc0e37d9edd1d2d7cdae7
Osman Taka <osmantaka@protonmail.com>
2022-03-15 12:24:25 +0300

    Update articles.dart

--- lib/articles.dart
+++ lib/articles.dart
@@ -15,16 +15,14 @@ class Articles {
           "The Refugee Problem",
           "https://im-media.voltron.voanews.com/Drupal/01live-166/styles/sourced/s3/2020-12/AP20338335728231.jpg?itok=Hr8r12zJ",
           <ArticleContent>[
-            ArticleContent("By Lisa",
-                "The refugee problem is a really big problem for every country in the world. All people should have the same living conditions from the beginning of their lives. Some people are lucky enough to be born in really good living conditions. Like being able to have a proper education, having a roof to live under, and more. But as the same amount of these lucky people, there are the same amount of unlucky people in this world. They can not afford to have proper education and even they can not afford to have a stable life under a roof. They have to leave their houses because of the wars around their living areas or because of the lack of food or water.\n\nIn the country you live in maybe you can have a 15 minute long shower but in some countries in Africa they can not find enough water to drink. Again when we are sick we can go to the doctors and reach for medicine but some people die just because of a simple flu because of the lack of health support.\n\nThese examples are really simple daily things for us and for the other 40 percent of humanity they are hard to reach. There are some solutions for these 40 percent of humanity. one of the primary solutions is to immigrate to other countries which have better living conditions. If they immigrate enough to some higher-level living condition city or country they can have the same living conditions as the rest of us. But in our century some countries do not accept refugees. Even from their neighbors. With this decision, they turn down many hopeless people so their citizens can have higher qualified lives. Many people from Europe, America, and Asia fight for the right of these people. As world citizens, we should not be silent about these topics. Even if you do not think about other peoplealways remember that life is not always going to stay positive for us and someday in the future, we may be in the same situation asthem. Before experiencing this horrible situation and living conditions we should start to legalize refugee acceptance and have an even word and fair life for every living person."),
+            ArticleContent("By Lisa", "The refugee problem is a really big problem for every country in the world. All people should have the same living conditions from the beginning of their lives. Some people are lucky enough to be born in really good living conditions. Like being able to have a proper education, having a roof to live under, and more. But as the same amount of these lucky people, there are the same amount of unlucky people in this world. They can not afford to have proper education and even they can not afford to have a stable life under a roof. They have to leave their houses because of the wars around their living areas or because of the lack of food or water.\n\nIn the country you live in maybe you can have a 15 minute long shower but in some countries in Africa they can not find enough water to drink. Again when we are sick we can go to the doctors and reach for medicine but some people die just because of a simple flu because of the lack of health support.\n\nThese examples are really simple daily things for us and for the other 40 percent of humanity they are hard to reach. There are some solutions for these 40 percent of humanity. one of the primary solutions is to immigrate to other countries which have better living conditions. If they immigrate enough to some higher-level living condition city or country they can have the same living conditions as the rest of us. But in our century some countries do not accept refugees. Even from their neighbors. With this decision, they turn down many hopeless people so their citizens can have higher qualified lives. Many people from Europe, America, and Asia fight for the right of these people. As world citizens, we should not be silent about these topics. Even if you do not think about other peoplealways remember that life is not always going to stay positive for us and someday in the future, we may be in the same situation asthem. Before experiencing this horrible situation and living conditions we should start to legalize refugee acceptance and have an even word and fair life for every living person."),
           ]
       ),
       ArticleModel(
           "What Is Citizenship ?",
           "https://images2.minutemediacdn.com/image/upload/c_crop,h_1125,w_2000,x_0,y_349/v1554993859/shape/mentalfloss/518685-istock-514930514.jpg?itok=qoapx9En",
           <ArticleContent>[
-            ArticleContent("By Greta Gilbertson",
-                "Citizenship is a multidimensional concept that means membership in a specific nation-state and the formal rights and obligations that this membership entails. Citizenship can also be understood as a status and an identity. The principle premise of citizenship is that nation-states can set and control the parameters of membership."),
+            ArticleContent("By Greta Gilbertson","Citizenship is a multidimensional concept that means membership in a specific nation-state and the formal rights and obligations that this membership entails. Citizenship can also be understood as a status and an identity. The principle premise of citizenship is that nation-states can set and control the parameters of membership."),
 
           ]
       ),
@@ -33,8 +31,7 @@ class Articles {
           "Who is a World Citizen ?",
           "https://www.kosmosjournal.org/wp-content/uploads/2018/12/connect-1024x512.png",
           <ArticleContent>[
-            ArticleContent("By Ambassador Choonghee Hahn",
-                "While it may mean different things to different people, the most common definition of a citizen of the world is the idea that all people have responsibilities to the world rather than just their country and immediate community.Global citizens are curious about human cultures, the environment and economics, and how they all connect with each other. They have a desire to contribute to communities and the world at large in a positive way in order to improve the lives of others."),
+            ArticleContent("By Ambassador Choonghee Hahn", "While it may mean different things to different people, the most common definition of a citizen of the world is the idea that all people have responsibilities to the world rather than just their country and immediate community.Global citizens are curious about human cultures, the environment and economics, and how they all connect with each other. They have a desire to contribute to communities and the world at large in a positive way in order to improve the lives of others."),
 
           ]
       ),
@@ -42,8 +39,7 @@ class Articles {
           "What is Racism ?",
           "https://pailondon.org.uk/wp-content/uploads/2020/09/dafddaec-2780-496c-954d-88091dfb204f.png",
           <ArticleContent>[
-            ArticleContent(" by Anup Shah",
-                "Racism is the belief that characteristics and abilities can be attributed to people simply on the basis of their race and that some racial groups are superior to others. Racism and discrimination have been used as powerful weapons encouraging fear or hatred of others in times of conflict and war, and even during economic downturns."),
+            ArticleContent(" by Anup Shah", "Racism is the belief that characteristics and abilities can be attributed to people simply on the basis of their race and that some racial groups are superior to others. Racism and discrimination have been used as powerful weapons encouraging fear or hatred of others in times of conflict and war, and even during economic downturns."),
           ]
 
       ),
@@ -51,24 +47,21 @@ class Articles {
           " The Impact of Social media on Society (Positive and Adverse Effects)",
           "https://blog-cw.s3.amazonaws.com/2018/07/social-media-tips-for-small-business.png",
           <ArticleContent>[
-            ArticleContent(" By ReadingJunction",
-                "Today, we are grappling with the shiny new Cs: innovation, connectivity, interaction, development (of new ideas and projects), society (with shared interests), partnership and (change of game) competitiveness.It made you wonder that anything so strong, but with such a massive reaches, hopefully, won’t be all well.\nJust as there are still two sides to a coin, and the same goes for social networks. Consequently, different cultures have different viewpoints on this controversial topic. Therefore, in this article on social networks, we’ll see the pros and cons impact of social media on society."),
+            ArticleContent(" By ReadingJunction","Today, we are grappling with the shiny new Cs: innovation, connectivity, interaction, development (of new ideas and projects), society (with shared interests), partnership and (change of game) competitiveness.It made you wonder that anything so strong, but with such a massive reaches, hopefully, won’t be all well.\nJust as there are still two sides to a coin, and the same goes for social networks. Consequently, different cultures have different viewpoints on this controversial topic. Therefore, in this article on social networks, we’ll see the pros and cons impact of social media on society."),
           ]
       ),
       ArticleModel(
           "Digital Citizenship During a Global Pandemic",
           "https://media.edutopia.org/styles/responsive_2880px_16x9/s3/masters/d7_images/cover_media/davis-169hero-citizenship-shutterstock.jpg",
           <ArticleContent>[
-            ArticleContent("By Beth A. Buchholz",
-                "When the coronavirus pandemic forced school closures and stay-at-home orders across the country, the internet became a social and academic lifeline for young people. Students are now engaging in distance learning daily. They’re also spending more time on social media as a way to stay connected to friends and family.This increase in the amount of time spent online also means there is greater potential for problems such as cyberbullying and other concerns.\nIt highlights the importance now more than ever of understanding and practicing good digital citizenship.Being a good digital citizen requires understanding how to be safe, respectful and informed in the online world. For example, it’s important to understand how to behave properly in an online classroom, how to identify “fake news” disseminated online, and how to engage in social media in a positive way.\nHere are some issues to consider, and some ways to promote good digital citizenship during the pandemic and beyond."),
+            ArticleContent("By Beth A. Buchholz", "When the coronavirus pandemic forced school closures and stay-at-home orders across the country, the internet became a social and academic lifeline for young people. Students are now engaging in distance learning daily. They’re also spending more time on social media as a way to stay connected to friends and family.This increase in the amount of time spent online also means there is greater potential for problems such as cyberbullying and other concerns.\nIt highlights the importance now more than ever of understanding and practicing good digital citizenship.Being a good digital citizen requires understanding how to be safe, respectful and informed in the online world. For example, it’s important to understand how to behave properly in an online classroom, how to identify “fake news” disseminated online, and how to engage in social media in a positive way.\nHere are some issues to consider, and some ways to promote good digital citizenship during the pandemic and beyond."),
           ]
       ),
       ArticleModel(
           " Why do we use Passports ?",
           "https://cdn.newsapi.com.au/image/v1/574ca670d3fa0892d778a9a9877be010",
           <ArticleContent>[
-            ArticleContent("By Brette Sember",
-                " It’s a legal ID If you lose your driver’s license, your passport serves as a legal photo ID for any situation that might require one. (However, it does not prove your legal driving status, should you get pulled over on a driving violation.)"),
+            ArticleContent("By Brette Sember"," It’s a legal ID If you lose your driver’s license, your passport serves as a legal photo ID for any situation that might require one. (However, it does not prove your legal driving status, should you get pulled over on a driving violation.)"),
           ]
       ),
       ArticleModel(
